d3a8253e0f4f4b3656da3af8ee41ddf07265dbb4
GiovanniS <giovanni.squillace.work@gmail.com>
2024-09-11 13:29:06 -0400

    add dynamic adding of menu items

--- restaurant_menu/views.py
+++ restaurant_menu/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views import generic
-from restaurant_menu.models import Item
+from restaurant_menu.models import Item, CATEGORY, AVAILABILITY
 
 # Create your views here.
 
@@ -8,8 +8,11 @@ class MenuList(generic.ListView):
 	queryset = Item.objects.order_by("creation_date")
 	template_name = "index.html"
 
-	def get_context_data(self):
-		context = {"meals": "pizza"}
+	def get_context_data(self, **kwargs):
+		context = super().get_context_data(**kwargs)
+		context["meals"] = CATEGORY
+		context["available"] = AVAILABILITY
+
 		return context
 
 
