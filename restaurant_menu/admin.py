1b1a018ff5846d5c326ad6321687541c608bd2e4
GiovanniS <giovanni.squillace.work@gmail.com>
2024-09-11 12:44:43 -0400

    add admin page and users. Also made some style changes

--- restaurant_menu/admin.py
+++ restaurant_menu/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from restaurant_menu.models import Item
 
 # Register your models here.
+
+class MenuItemAdmin(admin.ModelAdmin):
+	list_display = ("meal", "is_available")
+	list_filter = ("is_available", )
+	search_fields = ("meal", "desc")
+
+
+admin.site.register(Item, MenuItemAdmin)
--- restaurant_menu/models.py
+++ restaurant_menu/models.py
@@ -6,24 +6,24 @@ from django.contrib.auth.models import User
 CATEGORY = (
 	("starters", 'Starters'),
 	("salades", "Salades"),
-	("mean_dishes", "Mean Dishes"),
+	("main_dishes", "Main Dishes"),
 	("desserts", "Desserts")
 )
 
 AVAILABILITY = (
-	(0, "no"),
-	(1, "yes")
+	(0, "Sold Out"),
+	(1, "Available")
 )
 
 
 class Item(models.Model):
 	meal = models.CharField(max_length=200, unique=True)
 	desc = models.CharField(max_length=2000)
-	price = models.DecimalField(decimal_places=2, max_digits=2)
+	price = models.DecimalField(decimal_places=2, max_digits=5)
 	category = models.CharField(max_length=500, choices=CATEGORY)
 	# author = models.ForeignKey(User, on_delete=models.CASCADE)  # If the user is deleted from the tabel, all the items that user created would also be deleted
 	author = models.ForeignKey(User, on_delete=models.PROTECT)  # If the user is deleted from the tabel, none of its creations will be deleted
-	available = models.IntegerField(choices=AVAILABILITY, default=0)
+	is_available = models.IntegerField(choices=AVAILABILITY, default=0)
 	creation_date = models.DateTimeField(auto_now_add=True)
 	update_date = models.DateTimeField(auto_now=True)
 
